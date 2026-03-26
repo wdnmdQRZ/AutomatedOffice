6799b436c451e8eb97df70328b35c3e26d4dd551
秦瑞喆 <2814514743@qq.com>
2026-03-26 19:13:12 +0800

    新增：   +鼠标的移动，单双击，按下抬起 修改：   将“重复次数”改为“执行次数”，默认值从0改为1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,8 @@
 #include "./ui_mainwindow.h"
 #include <windows.h>
 #include <QTimer>
+#include <QPoint>
+#include <QMouseEvent>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -54,9 +56,19 @@ void MainWindow::on_comboBox_action_type_editTextChanged(const QString &arg1)
 {
     ui->comboBox_action_parameters->clear();
     if(arg1=="鼠标操作"){
-        ui->comboBox_action_parameters->addItems({"左键单击","右键单击","左键双击","右键双击","上滚滑轮","下滚滑轮"});
+        ui->comboBox_action_parameters->addItems({"左键按下","右键按下","左键抬起","右键抬起","左键单击","右键单击","左键双击","右键双击","上滚滑轮","下滚滑轮"});
     }
-    else if(arg1=="键盘操作"){
+    else if(arg1=="鼠标移动"){
+        ui->comboBox_action_parameters->addItems({"x,y"});
+    }
+    else if(arg1=="键盘按下"){
+        ui->comboBox_action_parameters->addItems({
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+            "u", "v", "w", "x", "y", "z"
+        });
+    }
+    else if(arg1=="键盘抬起"){
         ui->comboBox_action_parameters->addItems({
             "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
             "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
@@ -74,7 +86,7 @@ void MainWindow::on_pushButton_insert_clicked()
     //获取选项内容
     QString qs_action_type=this->ui->comboBox_action_type->currentText();
     QString qs_action_parameters=this->ui->comboBox_action_parameters->currentText();
-    long l_repeat_time=this->ui->spinBox_repeat_time->value();
+    long l_repeat_time=this->ui->spinBox_execute_time->value();
     long l_lastRow = ui->tableWidget_execut_table->rowCount();
     //插入内容
     ui->tableWidget_execut_table->insertRow(l_lastRow);
@@ -85,35 +97,54 @@ void MainWindow::on_pushButton_insert_clicked()
 
 void MainWindow::on_pushButton_run_clicked()
 {
-    QTableWidget *tableWidget_execut_table=this->ui->tableWidget_execut_table;
-    int rowCount = tableWidget_execut_table->rowCount();
-    int colCount = tableWidget_execut_table->columnCount();
+    QTableWidget *tableWidget_execut_table=this->ui->tableWidget_execut_table;  //指向表格
+    int rowCount = tableWidget_execut_table->rowCount();    //行数
+    int colCount = tableWidget_execut_table->columnCount(); //列数
     for(long i=0;i<rowCount;i++){
-        QString qs_type=tableWidget_execut_table->item(i,0)->text();
-        QString qs_parameters=tableWidget_execut_table->item(i,1)->text();
-        long l_repeat_time=tableWidget_execut_table->item(i,2)->text().toLong();
+        QString qs_type=tableWidget_execut_table->item(i,0)->text();    //获取操作类型
+        QString qs_parameters=tableWidget_execut_table->item(i,1)->text();  //具体操作数据
+        long l_execute_time=tableWidget_execut_table->item(i,2)->text().toLong();
         //鼠标操作
         if(qs_type=="鼠标操作"){
-            for(long j=0;j<l_repeat_time;j++){
+            for(long j=0;j<l_execute_time;j++){
+                if(qs_parameters == "左键按下") mouseLeftDown();
+                else if(qs_parameters == "右键按下") mouseRightDown();
+                else if(qs_parameters == "左键抬起") mouseLeftUp();
+                else if(qs_parameters == "右键抬起") mouseRightUp();
+                else if(qs_parameters == "左键单击") mouseLeftClick();
+                else if(qs_parameters == "右键单击") mouseRightClick();
+                else if(qs_parameters == "左键双击") mouseLeftDoubleClick();
+                else if(qs_parameters == "右键双击") mouseRightDoubleClick();
+            }
+        }
+        //鼠标移动
+        else if(qs_type=="鼠标移动"){
+            for(long j=0;j<l_execute_time;j++){
+                mouseMove(qs_parameters);
+            }
+        }
+        //键盘按下
+        else if(qs_type=="键盘按下"){
+            for(long j=0;j<l_execute_time;j++){
                 qDebug()<<qs_parameters;
             }
         }
-        //键盘操作
-        else if(qs_type=="键盘操作"){
-            for(long j=0;j<l_repeat_time;j++){
+        //键盘抬起
+        else if(qs_type=="键盘抬起"){
+            for(long j=0;j<l_execute_time;j++){
                 qDebug()<<qs_parameters;
             }
         }
         //延时操作
         else if(qs_type=="延时(毫秒)"){
-            for(long j=0;j<l_repeat_time;j++){
-                qDebug()<<qs_parameters;
+            for(long j=0;j<l_execute_time;j++){
                 _delay(qs_parameters.toInt());
             }
         }
     }
 }
 
+//局部事件非阻塞延时
 void MainWindow::_delay(int ms)
 {
     QEventLoop loop;
@@ -121,4 +152,79 @@ void MainWindow::_delay(int ms)
     loop.exec();
 }
 
+// 移动鼠标
+void MainWindow::mouseMove(const QString &posStr)
+{
+    QStringList list = posStr.split(",");
+    int x = list[0].toInt();
+    int y = list[1].toInt();
+    SetCursorPos(x, y);
+}
 
+// 左键按下
+void MainWindow::mouseLeftDown()
+{
+    INPUT input = {0};
+    input.type = INPUT_MOUSE;
+    input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+// 左键抬起
+void MainWindow::mouseLeftUp()
+{
+    INPUT input = {0};
+    input.type = INPUT_MOUSE;
+    input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+// 左键单击
+void MainWindow::mouseLeftClick()
+{
+    mouseLeftDown();
+    _delay(20);
+    mouseLeftUp();
+}
+
+// 左键双击
+void MainWindow::mouseLeftDoubleClick()
+{
+    mouseLeftClick();
+    _delay(100);
+    mouseLeftClick();
+}
+
+// 右键按下
+void MainWindow::mouseRightDown()
+{
+    INPUT input = {0};
+    input.type = INPUT_MOUSE;
+    input.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+// 右键抬起
+void MainWindow::mouseRightUp()
+{
+    INPUT input = {0};
+    input.type = INPUT_MOUSE;
+    input.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+// 右键单击
+void MainWindow::mouseRightClick()
+{
+    mouseRightDown();
+    _delay(20);
+    mouseRightUp();
+}
+
+// 右键双击
+void MainWindow::mouseRightDoubleClick()
+{
+    mouseRightClick();
+    _delay(100);
+    mouseRightClick();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMouseEvent>  // 必须加，否则鼠标事件报错
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,21 +18,28 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private:
+    void mouseMove(const QString &posStr);       // 移动
+    void mouseLeftDown();               // 左键按下
+    void mouseLeftUp();                 // 抬起
+    void mouseLeftClick();              // 单击
+    void mouseLeftDoubleClick();        // 双击
+    void mouseRightDown();              // 右键按下
+    void mouseRightUp();                // 抬起
+    void mouseRightClick();             // 单击
+    void mouseRightDoubleClick();       // 双击
+
 private slots:
     void on_pushButton_fixed_clicked();//固定页面
-
     void show_mouse_coordinates();//显示鼠标坐标
-
     void on_comboBox_action_type_editTextChanged(const QString &arg1);//根据不同类型显示不同操作
-
     void on_pushButton_insert_clicked();//插入数据
-
     void on_pushButton_run_clicked();//开始运行
-
     void _delay(int ms);//局部事件循环延时函数
 
 private:
     Ui::MainWindow *ui;
     bool isStayOnTop = false;   //判断窗口是否置顶
 };
+
 #endif // MAINWINDOW_H
